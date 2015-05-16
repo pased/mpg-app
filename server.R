ea842c2442608a4e0c120242e2f207a43c997e21
pased <pased@users.noreply.github.com>
2015-05-16 10:08:02 +0600

    a small clean up of the code

--- server.R
+++ server.R
@@ -32,10 +32,6 @@ shinyServer(function(input, output) {
   
   output$doc <- renderText({
 
-# p("The data was extracted from the 1974 
-#   Motor Trend US magazine, and comprises fuel consumption 
-#   and 10 aspects of automobile design and performance for 32 
-#   automobiles (1973–74 models).")
 })
 
 
